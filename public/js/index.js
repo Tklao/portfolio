338da32b519d2f098453333cfc225bd9d2fcc6c1
Tklao <carlosmelo1295@gmail.com>
2021-02-13 19:36:57 -0300

    organização de diretórios

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,12 @@
 function congratByName() {
     const $congrat = document.getElementById("congrat");
-    const getName = prompt("Digite seu nome")
-    return $congrat.innerHTML = `Muito Obrigado por sua visita, ${getName}🎉🎉🎉`
+    const getName = prompt("Digite seu nome");
+
+    if (getName == "" || getName == null) {
+        return ($congrat.innerHTML = `Muito Obrigado por sua visita!!!🎉`);
+    } else {
+        return ($congrat.innerHTML = `Muito Obrigado por sua visita, ${getName}🎉🎉🎉`);
+
+    }
 }
-congratByName()
+congratByName();
